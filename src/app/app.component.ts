a369b11d51b5305faff853fd9d7abca14d39c079
valdesik <valdesik000@gmail.com>
2021-04-26 11:03:58 +0300

    1
    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TimelineLite, TweenMax, Back, Power0, Linear, Bounce } from 'gsap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gsap-project';
+  animation = false;
+
+  public fOpeningAnim(): void {
+    this.animation = true;
+    TweenMax.to('#earth', 30, {
+      rotation: 360,
+      repeat: -1,
+      ease: Linear.easeNone
+    });
+    TweenMax.to( '#logo-text', 2, {scale: 20 });
+    TweenMax.to( '#astronaut', 2, {scale: 20 , ease: Bounce.easeOut, x: '370'  });
+
+    TweenMax.to('#astronaut', 2, {y: '-30', ease: Linear.easeNone, repeat: -1, yoyo: true});
+  }
 }
